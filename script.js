dde5af87c868a0625abe53f82d228407ef42f025
Leonardo Alves <leonardo.alves779@gmail.com>
2023-03-20 18:08:24 -0300

    creating sum time section

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // API Weather: https://open-meteo.com/en/docs#api_form
 // Layout: https://www.figma.com/community/file/1215291914714743267/%23boraCodar---Desafio-10
 
-// Minute: 52:00
+// Minute: 1:02:30
 // https://www.youtube.com/watch?v=qQoiVFML4sM&list=PL85ITvJ7FLohAe8-BUtOOJ7xyz089SH4m&index=1
