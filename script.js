3284b017055e295c7937631c744e152fc2469a71
Leonardo Alves <leonardo.alves779@gmail.com>
2023-03-25 20:46:38 -0300

    adding search field

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 // API Weather: https://open-meteo.com/en/docs#api_form
+// https://home.openweathermap.org/users/sign_up
+
 // Layout: https://www.figma.com/community/file/1215291914714743267/%23boraCodar---Desafio-10
+// https://www.youtube.com/watch?v=iILFBGm_I9M&list=WL&index=240&t=16s
+// defer on script
