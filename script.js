5e9c82a853ac93aa33f5cfe0219d57232f8424a4
Leonardo Alves <leonardo.alves779@gmail.com>
2023-03-27 13:10:48 -0300

    adding animations

--- script.js
+++ script.js
@@ -4,3 +4,7 @@
 // Layout: https://www.figma.com/community/file/1215291914714743267/%23boraCodar---Desafio-10
 // https://www.youtube.com/watch?v=iILFBGm_I9M&list=WL&index=240&t=16s
 // defer on script
+
+function teste(){
+    document.getElementById("x")
+}
