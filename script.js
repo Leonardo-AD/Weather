b29315ef262e737417e3edb8b115d7c21a675b0b
Leonardo Alves <leonardo.alves779@gmail.com>
2023-03-28 19:22:13 -0300

    adding sweet alert

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 // API Weather: https://open-meteo.com/en/docs#api_form
 // https://home.openweathermap.org/users/sign_up
 
-// Layout: https://www.figma.com/community/file/1215291914714743267/%23boraCodar---Desafio-10
 // https://www.youtube.com/watch?v=iILFBGm_I9M&list=WL&index=240&t=16s
-// defer on script
 
-function teste(){
-    document.getElementById("x")
-}
+
+// const searchInput = document.querySelector('#search-input')
+const searchButton = document.querySelector('#search-button')
+const weatherTemperature = document.querySelector('#weather-temperature')
+const windSpeed = document.querySelector('#wind-speed')
+const humidity = document.querySelector('#humidity')
+
+searchButton.addEventListener('click', () => {
+    const APIKey = 'email'
+    const city = document.querySelector('#search-input').value
+
+    if(!city || city == ''){
+        
+        Swal.fire({
+            title: "Não foi possível localizar",
+            text: "Por favor, digite novamente.",
+            icon: "error"
+          })
+    
+          return
+    }
+
+})
