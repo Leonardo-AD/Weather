be0842527487f1bd396ae488de39cbeae1dc2168
Leonardo Alves <leonardo.alves779@gmail.com>
2023-03-30 19:50:14 -0300

    implementing API connection

--- script.js
+++ script.js
@@ -2,18 +2,16 @@
 // https://home.openweathermap.org/users/sign_up
 
 // https://www.youtube.com/watch?v=iILFBGm_I9M&list=WL&index=240&t=16s
+// http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={APIkey} read the parameters
 
 // const searchInput = document.querySelector('#search-input')
 const searchButton = document.querySelector('#search-button')
-const weatherTemperature = document.querySelector('#weather-temperature')
-const windSpeed = document.querySelector('#wind-speed')
-const humidity = document.querySelector('#humidity')
 
 searchButton.addEventListener('click', () => {
-    const APIKey = 'email'
+    const APIKey = 'd5c9103c9c7225eb8814a372432fd7e9'
     const city = document.querySelector('#search-input').value
 
-    if(!city || city == ''){   // ALTERAR TAMANHO DO ALERT!!!
+    if(!city || city == ' '){   // ALTERAR TAMANHO DO ALERT!!!
         
         Swal.fire({
             title: "Não foi possível localizar",
@@ -23,8 +21,28 @@ searchButton.addEventListener('click', () => {
           })
     
           return
-    }else{
-        alert(APIKey)
     }
 
-})
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+    .then(res => res.json())
+    .then(json => {
+        
+        if(json.code === '404'){
+            
+            Swal.fire({
+                title: "Não foi possível localizar",
+                text: "Por favor, digite novamente.",
+                icon: "error",
+                background: "#E7E6FB"
+              })
+        
+              return
+        }
+
+        const citySelected = document.querySelector('#city-selected')
+        const weatherTemperature = document.querySelector('#weather-temperature')
+        const windSpeed = document.querySelector('#wind-speed')
+        const humidity = document.querySelector('#humidity')
+    })
+
+})  
